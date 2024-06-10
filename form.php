1754ef70d821d806f49c8549cac439087928b87f
Antonino <drakon9927@libero.it>
2024-06-10 14:54:20 +0200

    m1

---
+++ form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>formpage</title>
+</head>
+<body>
+    <h1>Funziona?</h1>
+  
+    <form action="index.php" method="GET">
+    <input type="text" placeholder="Scrivi il messaggio" name="messaggio">
+    <button type="submit">Invia</button>
+    </form>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$messaggio = $_GET["messaggio"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo $messaggio ?></h1>
+</body>
+</html>
